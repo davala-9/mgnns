033707fb3ced75abcac0cb21aa02d0e15da7af37
Filip Rehburg <filip.rehburg@student.uva.nl>
2026-07-15 13:59:31 +0200

    wrong edit, reverted

--- src/model/gnn_architectures.py
+++ src/model/gnn_architectures.py
@@ -23,7 +23,7 @@ class EC_GCNConv(MessagePassing):
 
     # in_channels (int) - Size of each input sample
     # out_channels (int) - Size of each output sample
-    def __init__(self, dimensions, edge_colours, aggregations):
+    def __init__(self, in_channels, out_channels, edge_colours, aggregation):
 
         self.aggr = aggregation
         aggr_name = getattr(aggregation, "value", aggregation)
